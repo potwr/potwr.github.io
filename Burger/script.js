20646f03e7e350c4f211e75dbf719149ce3a825b
potwr <86025006+potwr@users.noreply.github.com>
2024-05-30 21:52:04 +0200

    Add files via upload

--- Burger/script.js
+++ Burger/script.js
@@ -14,15 +14,39 @@ var ingredientsList = [
     {name: "veggie burger", key: "KeyV", penalty: 3}, //12
     {name: "mustard", key: "KeyU", penalty: 1}, //13
     {name: "original sauce", key: "KeyR", penalty: 1}, //14
-]
+];
+
+// {name: , key: , penalty: },
 
 var beverageMenu = [
     {name: "Coke", key: "Digit1"},
     {name: "Coke Zero", key: "Digit2"},
     {name: "Fanta", key: "Digit3"},
-    {name: "Sprite", key: "Digit4"}
-]
-// {name: , key: , penalty: },
+    {name: "Sprite", key: "Digit4"},
+//    {name: "Orange Juice", key: "Digit5"},
+//    {name: "Apple Juice", key: "Digit6"},
+//    {name: "Beer", key: "Digit7"}
+];
+
+var sidesMenu = [
+    {name: "Fries, small", key: "Digit9"},
+    {name: "Fries, big", key: "Digit0"}
+];
+
+var ingredientsKeysReserved = [];
+for(num = 0; num < ingredientsList.length; num++) {
+    ingredientsKeysReserved.push(ingredientsList[num].key);
+}
+
+var beverageKeysReserved = [];
+for(num = 0; num < beverageMenu.length; num++) {
+    beverageKeysReserved.push(beverageMenu[num].key);
+}
+
+var sidesKeysReserved = [];
+for(num = 0; num < sidesMenu.length; num++) {
+    sidesKeysReserved.push(sidesMenu[num].key);
+}
 
 var burgerMenu = [
     {name: "Frisco", ingredients: [ingredientsList[14], ingredientsList[2], ingredientsList[7], ingredientsList[5], ingredientsList[9], ingredientsList[0], ingredientsList[11]]},
@@ -65,7 +89,7 @@ function createOrder() {
     
     $(".order-"+orderSlot+" .order-content").append('<span class="order-name">'+burgerMenu[menuNumber].name+'</span><br>');
 
-    orderCompositions.push({orderNumber: orderCount, ingredients: [ingredientsList[1]], beverages: [], beveragesOrdered: false, ingredientsPrepared: false, beveragesPrepared: false});
+    orderCompositions.push({orderNumber: orderCount, ingredients: [ingredientsList[1]], beverages: [], sides: [], beveragesOrdered: false, sidesOrdered: false, ingredientsPrepared: false, beveragesPrepared: false, sidesPrepared: false});
 
     for(a = 0; a < ingredientsNumber; a++) {
         $(".order-"+orderSlot+" .order-content").append('<span class="order-ingredient order-ingredient-'+(a+1)+'">'+burgerMenu[menuNumber].ingredients[a].name+'</span><br>');
@@ -80,8 +104,22 @@ function createOrder() {
         orderCompositions[orderCount-1].beverages.push(beverageMenu[beverageNumber]);
         orderCompositions[orderCount-1].beveragesOrdered = true;
     }
-    else {
+    
+    let isSide = randomNumber(10);
+    if(isSide < 8) {
+        let sideNumber = randomNumber(sidesMenu.length);
+        $(".order-"+orderSlot+" .order-content").append('<span class="order-side">'+sidesMenu[sideNumber].name+'</span><br>');
+        orderCompositions[orderCount-1].sides.push(sidesMenu[sideNumber]);
+        orderCompositions[orderCount-1].sidesOrdered = true;
+    }
 
+    if(numberOfCurrentOrders == 0) {
+        clearInterval(automaticFewerPoints);
+        automaticFewerPoints = setInterval(function() {
+            fewerPointsForOrder(1);
+        }, automaticFewerPointsInterval);
+        currentOrderNumberPreparedPoints = 10;
+        $(".kitchen-points-number").html(currentOrderNumberPreparedPoints);
     }
 
     numberOfCurrentOrders++;
@@ -133,11 +171,91 @@ function isKeyInBeveragesList(e) {
     }
 }
 
-function isCurrentIgredientCorrect(e) {
+function isKeyInSidesList(e) {
+    let sidesLength = sidesMenu.length;
+
+    let a = 0;
+    while(a < sidesLength) {
+        if(e.code === sidesMenu[a].key && orderCompositions[currentOrderNumberPrepared-1].sidesPrepared == false) {
+            let sideSize;
+            switch(a) {
+                case 0:
+                    sideSize = "small";
+                    break;
+
+                case 1:
+                    sideSize = "big";
+                    break;
+            }
+
+            $(".kitchen-cooking").append('<div class="side"></div>');
+
+            for(b = 1; b <= 6; b++) {
+                $(".side").append('<div class="side-fry side-fry'+b+' side-fry-'+sideSize+'"></div>');
+            }
+
+            $(".side").append('<div class="side-bag side-bag-'+sideSize+'"><span class="side-name">Crispy Fries</span></div>');
+
+            a = sidesLength+1;
+            return true;
+        }
+        a++;
+    }
+    if(a == sidesLength) {
+        return false;
+    }
+}
+
+function isCurrentIngredientCorrect(e) {
+    let currentOrder = orderCompositions[currentOrderNumberPrepared-1];
+    let currentIngredient = orderCompositions[currentOrderNumberPrepared-1].ingredients[currentIngredientPrepared];
+
+    if(ingredientsKeysReserved.includes(e.code) && currentOrder.ingredientsPrepared == false) {
+        if(e.code == currentIngredient.key) {
+            $(".order-"+currentOrderNumberPrepared+" .order-ingredient-"+currentIngredientPrepared).css("color", "lightgreen");
+            currentIngredientPrepared++;
+        }
+        else {
+            let errorReason = (currentIngredientPrepared == 0) ? "no bun" : "wrong ingredient";
+            $(".order-"+currentOrderNumberPrepared+" .order-ingredient-"+currentIngredientPrepared).css("color", "red");
+            fewerPointsForOrder(currentIngredient.penalty, errorReason);
+            currentIngredientPrepared++;
+        }
+    }
+
+    if(beverageKeysReserved.includes(e.code) && currentOrder.beveragesPrepared == false) {
+        if(currentOrder.beveragesOrdered == true && e.code == currentOrder.beverages[0].key) {
+            $(".order-"+currentOrderNumberPrepared+" .order-beverage").css("color", "lightgreen");
+        }
+        else if(currentOrder.beveragesOrdered == true && e.code != currentOrder.beverages[0].key) {
+            $(".order-"+currentOrderNumberPrepared+" .order-beverage").css("color", "red");
+            fewerPointsForOrder(2, 'wrong beverage');
+        }
+        else {
+            fewerPointsForOrder(2, "no beverage in the receipt");
+        }
+    }
+
+    if(sidesKeysReserved.includes(e.code) && currentOrder.sidesPrepared == false) {
+        if(currentOrder.sidesOrdered == true && e.code == currentOrder.sides[0].key) {
+            $(".order-"+currentOrderNumberPrepared+" .order-side").css("color", "lightgreen");
+        }
+        else if(currentOrder.sidesOrdered == true && e.code != currentOrder.sides[0].key) {
+            $(".order-"+currentOrderNumberPrepared+" .order-side").css("color", "red");
+            fewerPointsForOrder(2, 'wrong fries size');
+        }
+        else {
+            fewerPointsForOrder(2, "no fries in the receipt");
+        }
+    }
+
+}
+
+function isCurrentIngredientCorrectOld(e) {
     let currentIngredient = orderCompositions[currentOrderNumberPrepared-1].ingredients[currentIngredientPrepared];
     if(orderCompositions[currentOrderNumberPrepared-1].ingredientsPrepared == false) {
         if(e.code !== currentIngredient.key && orderCompositions[currentOrderNumberPrepared-1].beveragesOrdered == true && orderCompositions[currentOrderNumberPrepared-1].beveragesPrepared == false) {
-            if(e.code === "Digit1" || e.code === "Digit2" || e.code === "Digit3" || e.code === "Digit4") {
+            if(beverageKeysReserved.includes(e.code)) {
                 if(e.code === orderCompositions[currentOrderNumberPrepared-1].beverages[0].key) {
                     $(".order-"+currentOrderNumberPrepared+" .order-beverage").css("color", "lightgreen");
                 }
@@ -176,7 +294,7 @@ function isCurrentIgredientCorrect(e) {
         }
     }
     else if(orderCompositions[currentOrderNumberPrepared-1].ingredientsPrepared == true && orderCompositions[currentOrderNumberPrepared-1].beveragesPrepared == false) {
-        if(e.code === "Digit1" || e.code === "Digit2" || e.code === "Digit3" || e.code === "Digit4") {
+        if(beverageKeysReserved.includes(e.code)) {
             if(orderCompositions[currentOrderNumberPrepared-1].beveragesOrdered == false) {
                 fewerPointsForOrder(2, "no beverage in the receipt");
             }
@@ -244,8 +362,8 @@ document.addEventListener("keydown", function (e) {
     if(currentIngredientPrepared < orderCompositions[currentOrderNumberPrepared-1].ingredients.length) { 
         if(isKeyInIngredientsList(e) == true) {
             $(".ingredient").first().animate({top: "+="+lengthToDownAnimation+"px"}, 100 );
-            lengthToDownAnimation -=40;
-            isCurrentIgredientCorrect(e);
+            lengthToDownAnimation -= (parseInt($(".ingredient").css("height").slice(0, 2)) + parseInt($(".ingredient").css("padding-top").slice(0, 2)) + parseInt($(".ingredient").css("padding-bottom").slice(0, 2)));
+            isCurrentIngredientCorrect(e);
         }
     }
     else {
@@ -253,9 +371,15 @@ document.addEventListener("keydown", function (e) {
     }
 
     if(isKeyInBeveragesList(e) == true) {
-       $(".beverage").last().animate({right: "+=250px"}, 100 );
-       isCurrentIgredientCorrect(e);
+       $(".beverage").animate({right: "+=230px"}, 100 );
+       isCurrentIngredientCorrect(e);
        orderCompositions[currentOrderNumberPrepared-1].beveragesPrepared = true;
+    }
+
+    if(isKeyInSidesList(e) == true) {
+        $(".side").animate({left: "50px"}, 100);
+        isCurrentIngredientCorrect(e);
+        orderCompositions[currentOrderNumberPrepared-1].sidesPrepared = true;
     }
 
     if(e.code == "Enter") {
@@ -317,4 +441,20 @@ var automaticFewerPoints = setInterval(function() {
         $(".instruction-beverages table").append("<tr><td>["+beverageMenu[a].key.slice(-1)+"]</td><td>"+beverageMenu[a].name+"</td><td>2 pts</td></tr>");
     }
 
+    //fries
+    for(a = 0; a < sidesMenu.length; a++) {
+        $(".instruction-fries table").append("<tr><td>["+sidesMenu[a].key.slice(-1)+"]</td><td>"+sidesMenu[a].name+"</td><td>2 pts</td></tr>");
+    }
+
+    //menu
+    for(a = 0; a < burgerMenu.length; a++) {
+        $(".instruction-menu").append('<span class="instruction-menu-burger-name">'+burgerMenu[a].name+'</span><span class="instruction-menu-burger-ingredients">');
+        for(b = 0; b < burgerMenu[a].ingredients.length; b++) {
+            $(".instruction-menu").append(burgerMenu[a].ingredients[b].name);
+            if(b != (burgerMenu[a].ingredients.length-1)) {
+                $(".instruction-menu").append(", ");
+            }
+        }
+        $(".instruction-menu").append("</span>");
+    }
  }
